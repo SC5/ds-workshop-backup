f6c7876de15f788a687bf320d44a15ed692f05ce
Varya Stepanova <mail@varya.me>
2018-01-30 17:28:05 +0200

    Typography

---
+++ src/Typography/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import * as s from './styles.js'
+
+const Typography = (props) => {
+  return (
+    <s.Typography>
+      <s.FuturaBold>In Futura Bold: The quick brown fox jumps over the lazy dog.</s.FuturaBold>
+      <s.Lato>In Lato: The quick brown fox jumps over the lazy dog.</s.Lato>
+    </s.Typography>
+  )
+}
+
+export default Typography
